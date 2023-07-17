c93b9435f6a707452033b1c20fb97b6d1c2059c9
theartofnonso <slimbion@gmail.com>
2023-07-17 11:29:14 +0100

    updated logo widget

--- lib/logo_widget.dart
+++ lib/logo_widget.dart
@@ -1,33 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LogoWidget extends StatelessWidget {
   const LogoWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          width: 20,
-          height: 10,
-          color: Colors.white,
+        Text("Tracker",
+            style: GoogleFonts.pathwayExtreme(
+                fontWeight: FontWeight.w700,
+                fontSize: 18,
+                color: Colors.white)),
+        Row(
+          children: [
+            Container(
+              width: 30,
+              height: 10,
+              color: Colors.white,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Container(
+              width: 10,
+              height: 10,
+              color: Colors.white,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Container(
+              width: 10,
+              height: 10,
+              color: Colors.white,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Container(
+              width: 10,
+              height: 10,
+              color: Colors.white,
+            )
+          ],
         ),
-        const SizedBox(
-          width: 5,
-        ),
-        Container(
-          width: 10,
-          height: 10,
-          color: Colors.white,
-        ),
-        const SizedBox(
-          width: 5,
-        ),
-        Container(
-          width: 10,
-          height: 10,
-          color: Colors.white,
-        )
       ],
     );
   }
