4c544b1007508c7f1d484a66f2c4d391dc4356cf
theartofnonso <23728678+theartofnonso@users.noreply.github.com>
2024-02-16 15:34:11 +0000

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Trackr",
+      title: "TKRK",
       theme: ThemeData(
         scaffoldBackgroundColor: tealBlueDark,
         colorScheme: const ColorScheme(
